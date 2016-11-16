dc6dbdf2ee607957fe5b38036e0e111ca5c5ea49
blacksmith4393 <zach.smith33@gmail.com>
2016-11-15 23:08:48 -0500

    adding/changing more styling, added new logic to javascript

--- javascript.js
+++ javascript.js
@@ -1,47 +1,61 @@
+// ---------- Variables---------
+function myFunction(){
+  // Store array of images
+  var images = ['android', 'alarm', 'cloud', 'delete', 'android', 'alarm', 'cloud', 'delete'];
 
+  // Store #gameBoard ul element
+  var gameBoard = document.getElementById('gameBoard');
 
-var tileColors = ['red', 'red', 'blue', 'blue', 'green', 'green', 'orange', 'orange'];
-var gameBoard = $('ul');
-var blue = 'blue';
+  // Store shuffle button element
+  var shuffleButton = document.getElementById('shuffleButton');
 
-//Fisher–Yates shuffle function From http://stackoverflow.com/a/2450976
-function shuffle(array){
-	var unshuffled = array.length, temporary, picked;
-	//While there are unshuffled elements
-	while(unshuffled){
+  // ---------- Functions ----------
 
-		//Pick a random unshuffled element
-		picked = Math.floor(Math.random() * unshuffled--);
-		
+  function showAlert() {
+    alert("clicked");
+  }
 
-		//Swap picked element with last element
-		temporary = array[unshuffled];
-		array[unshuffled] = array[picked];
-		array[picked] = temporary;
+  function removeChildren(node) {
+    var fc = node.firstChild;
+    while( fc ) {
+        node.removeChild( fc );
+        fc = node.firstChild;
+    }
+  }
 
-	}
-	return array;
+  function shuffle(arr){
+    var m = arr.length, t, i;
+    // While there remain elements to shuffle…
+    while(m){
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+      // And swap it with the current element
+      t = arr[m];
+      arr[m]=arr[i];
+      arr[i] = t;
+    }
+    return arr;
+  }
+  //
+  function init(){
+    // Shuffle images/images
+  	shuffle(images);
+    // Append tiles to gameboard
+  	for ( var i = 0; i < images.length; i++) {
+  	    var li = document.createElement("li");
+  	    li.className = images[i];
+  	    var textnode = '<i class="material-icons md-48">' + (images[i]) + '</i>';
+  	    li.innerHTML = textnode;
+  	    gameBoard.appendChild(li);
+  	}
+  }
+
+  // ---------- Main ----------
+  // Initiate list items in gameboard
+  init();
+
+  shuffleButton.addEventListener("click", function(){
+    removeChildren(gameBoard);
+    init();
+  });
 }
-
-
-
-
-//Add onclick even listener to all tiles 
-$(document).ready(function(){
-
-	shuffle(tileColors);
-
-	gameBoard.append($('<li class="tile ' + blue + '"><p>blue</p></li>'));
-
-
-
-	$(".tile").on("click", function(){
-		console.log("clicked");
-	});
-
-
-
-});
-
-// 1. Click button
-// 2. Click second button
